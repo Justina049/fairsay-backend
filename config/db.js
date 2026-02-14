6a6854ef77908022527f475753bff1ecf61c0e1f
Justina049 <enenchejustina@gmail.com>
2026-02-14 18:07:01 +0100

    updated the database connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+//   host: "localhost",
+  host : '127.0.0.1',
+  user: "justina",
   password: "",
-  database: "fairsay_db"
+  database: "fairsay_db",
+  connectionLimit : 10,
 });
 
 db.connect(err => {
