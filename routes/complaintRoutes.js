62bb29d4644add6b10de4729a9a85784db736972
Justina049 <enenchejustina@gmail.com>
2026-02-17 06:30:37 +0100

    feat(complaints): add admin route to fetch all complaints

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const verifyToken = require("../middleware/authMiddleware");
-const { submitComplaint } = require("../controllers/complaintController");
+const { submitComplaint, getAllComplaints } = require("../controllers/complaintController");
 
 router.post("/", verifyToken, submitComplaint);
-
-// console.log("verifyToken:", verifyToken);
-// console.log("submitComplaint:", submitComplaint);
+router.get("/", verifyToken, getAllComplaints);
 
 
 module.exports = router;
