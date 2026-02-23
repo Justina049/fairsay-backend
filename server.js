350d3fdb6b6106697fce2271cbd821c641364911
Justina049 <enenchejustina@gmail.com>
2026-02-24 00:12:37 +0100

    commented global error handler

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get("/", (req, res) => {
   res.send("FairSay API Running");
 });
 
+// Health / Ping Endpoint
+app.get("/ping", (req, res) => {
+  res.status(200).send("OK");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
