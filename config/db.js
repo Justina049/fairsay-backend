cd1010f12b7c81a8440a48043fb18fa27f151119
Justina049 <enenchejustina@gmail.com>
2026-02-25 05:53:28 +0100

    Add SSL for Railway

--- config/db.js
+++ config/db.js
@@ -29,12 +29,16 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 console.log("MySQL Pool Created");
